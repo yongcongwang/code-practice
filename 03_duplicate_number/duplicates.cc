5e1fc2734a93a78b3346518b67f61645c5efec60
yongcong.wang <yongcong.wang@outlook.com>
2020-04-24 23:52:18 +0800

    add code

--- 03_duplicate_number/duplicates.cc
+++ 03_duplicate_number/duplicates.cc
@@ -25,9 +25,43 @@ int GetDuplicateNum(std::vector<int> &arr) {
   return -1;
 }
 
+int NumCnt(const std::vector<int> &arr, const int start, const int end) {
+  int cnt(0);
+  for (auto num : arr) {
+    if (num >= start && num <=end) {
+      cnt++;
+    }
+  }
+  return cnt;
+}
+  
+int GetRangeDuplication(const std::vector<int> &arr, const int start,
+                        const int end) {
+  if (start == end) {
+    return -1;
+  }
+
+  if (end - start == 1) {
+    const int cnt_start = NumCnt(arr, start, start);
+    return cnt_start > 1 ? start : end;
+  }
+
+  const int mid = (start + end) / 2;
+  return NumCnt(arr, start, mid) > mid - start + 1 ? 
+      GetRangeDuplication(arr, start, mid) : GetRangeDuplication(arr, mid, end);
+}
+
+int GetDuplicatedNum(const std::vector<int> &arr) {
+  if (arr.empty()) {
+    return -1;
+  }
+
+  return GetRangeDuplication(arr, 0, arr.size() - 1);
+}
+
 int main() {
-  std::vector<int> test1 = {2, 3, 5, 0, 2, 4, 1};
-  std::cout << GetDuplicateNum(test1) << std::endl;
+  std::vector<int> test1 = {2, 3, 5, 0, 5, 4, 1};
+  std::cout << GetDuplicatedNum(test1) << std::endl;
 
   std::vector<int> test2 = {3, 3, 3, 3, 3, 3, 3};
   std::cout << GetDuplicateNum(test2) << std::endl;
