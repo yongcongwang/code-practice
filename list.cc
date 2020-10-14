827973e7f40ca778c5476dfb1562a403c8ea6177
yongcong.wang <yongcong.wang@outlook.com>
2020-10-15 00:54:10 +0800

    add functions

--- list.cc
+++ list.cc
@@ -17,19 +17,106 @@ class List {
   class const_iterator {
    public:
     const_iterator() : current{} {}
+
+    const Object& operator*() const {
+      return retrieve();
+    }
+    const_iterator& operator++() {
+      current = current->next;
+      return *this;
+    }
+
+    const_iterator operator++(int) {
+      const_iterator old = *this;
+      ++(*this);
+      return old;
+    }
+
+    bool operator==(const const_iterator& rhs) const {
+      return current == rhs.current;
+    }
+
+    bool operator!=(const const_iterator& rhs) const {
+      return !(*this == rhs);
+    }
+
    protected:
     Node* current;
-    Object& retrieve() 
+
+    Object& retrieve() {
+      return current->val;
+    }
+
+    const_iterator(Node* p) : current(p) {}
+
+    friend class List<Object>;
   };
-  class iterator : public const_iterator {};
+
+  class iterator : public const_iterator {
+   public:
+    iterator() = default;
+
+    Object& operator*() {
+      return const_iterator::retrieve();
+    }
+
+    const Object& operator*() const {
+      return const_iterator::operator*();
+    }
+
+    iterator &operator++() {
+      this->current = this->current->next;
+      return *this;
+    }
+
+    iterator operator++(int) {
+      iterator old = *this;
+      ++(*this);
+      return old;
+    }
+
+   protected:
+    iterator(Node* p) : const_iterator(p) {}
+
+    friend class List<Object>;
+  };
 
  public:
-  List() {}
-  List(const List& rhs) {}
-  ~List() {}
-  List& operator=(const List& rhs) {}
-  List(const List&& rhs) {}
-  List& operator=(const List&& rhs) {}
+  List() {
+    init();
+  }
+  ~List() {
+    clear();
+    delete head;
+    delete tail;
+  }
+
+  List(const List& rhs) {
+    init();
+    for (auto& x : rhs) {
+      push_back(x)
+    }
+  }
+  
+  List& operator=(const List& rhs) {
+    List copy = rhs;
+    std::swap(*this, copy);
+    return *this;
+  }
+
+  List(const List&& rhs)
+      : size_{rhs.size_}, head_(rhs.head_), tail_(rhs.tail_) {
+    rhs.size_ = 0;
+    rhs.head_ = nullptr;
+    rhs.tail_ = nullptr;
+  }
+
+  List& operator=(const List&& rhs) {
+    std::swap(size_, rhs.size_);
+    std::swap(head_, rhs.head_);
+    std::swap(tail_, rhs.tail_);
+    return *this;
+  }
 
  public:
   iterator begin() {
