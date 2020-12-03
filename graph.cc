342b6f3e2a91b72738b6050c9766658e6b474040
yongcong.wang <yongcong.wang@outlook.com>
2020-12-03 21:44:28 +0800

    fix bugs

--- graph.cc
+++ graph.cc
@@ -1,29 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <unordered_set>
-
-template <typename T>
-struct Hasher;
-template <typename T>
-struct Vertex;
-
-template <typename T>
-struct Edge {
-  Vertex<T>* to{nullptr};
-  T weight{0};
-
-  bool operator==(const Edge<T>& rhs) {
-    return this->to == rhs.to && this->weight == rhs.weight;
-  }
-};
 
 template <typename T>
 struct Vertex {
   T data{};
-  std::unordered_set<Edge<T>, Hasher<T>> edges{};
 
   bool operator==(const Vertex<T>& rhs) const {
     return this->data == rhs.data;
+  }
+};
+
+template <typename T>
+struct Edge {
+  Vertex<T> from{};
+  Vertex<T> to{};
+  T weight{0};
+
+  bool operator==(const Edge<T>& rhs) const {
+    return from == rhs.from && to == rhs.to;
   }
 };
 
@@ -48,38 +44,36 @@ class Graph {
 
  public:
   void add_vertex(const Vertex<T>& ver) {
-    if (auto res = vertexes_.find(ver); res == vertexes_.end()) {
-      vertexes_.insert(ver);
+    if (auto res = ver_edges_.find(ver); res == ver_edges_.end()) {
+      ver_edges_.insert(ver, {});
     }
   }
   void add_vertex(const T& val) {
-    add_vertex({val, {}});
+    add_vertex({val});
   }
   void delete_vertex(const Vertex<T>& ver) {
-    if (auto res = vertexes_.find(ver); res != vertexes_.end()) {
-      vertexes_.erase(res);
+    if (auto res = ver_edges_.find(ver); res != ver_edges_.end()) {
+      ver_edges_.erase(res);
     }
   }
   void delete_vertex(const T& ver) {
-    delete_vertex({ver, {}});
+    delete_vertex({ver});
   }
 
   void add_directed_edge(const Vertex<T>& from, const Vertex<T>& to,
                          const T& weight) {
-    auto res_from = vertexes_.find(from);
-    auto res_to = vertexes_.find(to);
-    if (res_from != vertexes_.end() && res_to != vertexes_.end()) {
-      res_from->edges.insert({&(*res_to), weight});
+    if (auto res = ver_edges_.find(from); res != ver_edges_.end()) {
+      res->second.emplace(from, to, weight);
     }
   }
   void add_directed_edge(const T& from, const T& to, const T& weight) {
-    add_directed_edge({from, {}}, {to, {}}, weight);
+    add_directed_edge({from}, {to}, weight);
   }
   void delete_directed_edge(const Vertex<T>& from, const Vertex<T>& to) {
-    if (auto res_from = vertexes_.find(from); res_from != vertexes_.end()) {
-      auto& edges = res_from->edges;
-      if (auto res_to = edges.find(to); res_to != edges.end()) {
-        edges.erase(res_to);
+    if (auto res = ver_edges_.find(from); res != ver_edges_.end()) {
+      auto& edges = res->second;
+      if (auto edge = edges.find({from, to}); edge != edges.end()) {
+        edges.erase(edge);
       }
     }
   }
@@ -103,16 +97,17 @@ class Graph {
   }
 
   void print_adjacency_list() const {
-    for (const auto& ver : vertexes_) {
-      std::cout << "[" << ver.data << "]:" << std::endl;
-      for (const auto& e : ver.edges) {
-        std::cout << " -> " << e.to->data << std::endl;
+    for (const auto& v_e : ver_edges_) {
+      std::cout << "[" << v_e.first.data << "]:" << std::endl;
+      for (const auto& e : v_e.second) {
+        std::cout << " -> " << e.to.data << std::endl;
       }
     }
   }
 
  private:
-  std::unordered_set<Vertex<T>, Hasher<T>> vertexes_;
+  std::unordered_map<
+      Vertex<T>, std::unordered_set<Edge<T>, Hasher<T>>, Hasher<T>> ver_edges_;
 };
 
 int main() {
@@ -126,7 +121,6 @@ int main() {
   graph.delete_vertex(3);
   graph.delete_vertex(3);
 
-  Edge<int> edge{nullptr, 0};
   graph.add_directed_edge(1, 3, 0);
 
   graph.print_adjacency_list();
